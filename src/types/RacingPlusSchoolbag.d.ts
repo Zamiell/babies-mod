eed2e13f635c429a198756e635c31c4a314bdece
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-03-22 20:43:24 -0400

    trim

--- src/types/RacingPlusSchoolbag.d.ts
+++ src/types/RacingPlusSchoolbag.d.ts
@@ -1,6 +1,5 @@
 declare const RacingPlusSchoolbag: {
   addCharge(onlyOneCharge: boolean): void;
-  isItemFullyCharged(): boolean;
   put(collectibleType: int, charge: int): void;
   remove(): void;
 };
