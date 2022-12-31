0890764946b36b248d901b17126e5de6adcc1830
James <5511220+Zamiell@users.noreply.github.com>
2022-12-31 14:49:06 -0500

    fix: unnecessary item tracker removal

--- src/callbacksCustom/postItemPickupBabyFunctionMap.ts
+++ src/callbacksCustom/postItemPickupBabyFunctionMap.ts
@@ -1,8 +1,5 @@
 import { ItemType } from "isaac-typescript-definitions";
-import {
-  PickingUpItem,
-  removeCollectibleFromItemTracker,
-} from "isaacscript-common";
+import { PickingUpItem } from "isaacscript-common";
 import { RandomBabyType } from "../enums/RandomBabyType";
 import { TELEPORT_COLLECTIBLE_TYPE_TO_ROOM_TYPE_MAP } from "../maps/teleportCollectibleTypeToRoomTypeMap";
 
@@ -24,7 +21,6 @@ postItemPickupBabyFunctionMap.set(
     );
     if (teleportRoomType !== undefined) {
       player.RemoveCollectible(pickingUpItem.subType);
-      removeCollectibleFromItemTracker(pickingUpItem.subType);
     }
   },
 );
