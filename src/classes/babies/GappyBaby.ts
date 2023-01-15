481e938cf01a9d33a650de5632a30c0a03584f50
James <5511220+Zamiell@users.noreply.github.com>
2023-01-15 11:28:26 -0500

    chore: adding xml validation

--- src/classes/babies/GappyBaby.ts
+++ src/classes/babies/GappyBaby.ts
@@ -1,5 +1,9 @@
 import { CollectibleType } from "isaac-typescript-definitions";
-import { CallbackCustom, ModCallbackCustom } from "isaacscript-common";
+import {
+  CallbackCustom,
+  isSlotMachine,
+  ModCallbackCustom,
+} from "isaacscript-common";
 import { g } from "../../globals";
 import { mod } from "../../mod";
 import { Baby } from "../Baby";
@@ -8,6 +12,10 @@ import { Baby } from "../Baby";
 export class GappyBaby extends Baby {
   @CallbackCustom(ModCallbackCustom.POST_SLOT_DESTROYED)
   postSlotDestroyed(slot: EntitySlot): void {
+    if (!isSlotMachine(slot)) {
+      return;
+    }
+
     mod.spawnCollectible(CollectibleType.NULL, slot.Position, g.run.rng);
   }
 }
