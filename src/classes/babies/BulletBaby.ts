3cf692bb470577e328f58ee1f1beb19c343d9a0a
James <5511220+Zamiell@users.noreply.github.com>
2023-01-16 19:37:56 -0500

    fix: infinite bombs --> golden bomb

--- src/classes/babies/BulletBaby.ts
+++ src/classes/babies/BulletBaby.ts
@@ -1,29 +1,8 @@
-import { CallbackCustom, ModCallbackCustom } from "isaacscript-common";
-import { g } from "../../globals";
 import { Baby } from "../Baby";
 
 /** Starts with Rocket in a Jar + infinite bombs + blindfolded. */
 export class BulletBaby extends Baby {
-  override isValid(player: EntityPlayer): boolean {
-    const bombs = player.GetNumBombs();
-    return bombs < 50;
-  }
-
   override onAdd(player: EntityPlayer): void {
-    const numBombs = player.GetNumBombs();
-
-    g.run.babyCounters = numBombs; // Store the bomb count so that we can restore it later.
-    player.AddBombs(99);
-  }
-
-  /** Restore the bomb count to what it was before we got this baby. */
-  override onRemove(player: EntityPlayer, oldBabyCounters: number): void {
-    player.AddBombs(-99);
-    player.AddBombs(oldBabyCounters);
-  }
-
-  @CallbackCustom(ModCallbackCustom.POST_PEFFECT_UPDATE_REORDERED)
-  postPEffectUpdateReordered(player: EntityPlayer): void {
-    player.AddBombs(1);
+    player.AddGoldenBomb();
   }
 }
--- src/classes/babies/RageBaby.ts
+++ src/classes/babies/RageBaby.ts
@@ -1,30 +1,8 @@
-import { CallbackCustom, ModCallbackCustom } from "isaacscript-common";
-import { g } from "../../globals";
 import { Baby } from "../Baby";
 
-/** Starts with Sad Bombs + infinite bombs + blindfolded. */
+/** Starts with Sad Bombs + golden bomb + blindfolded. */
 export class RageBaby extends Baby {
-  override isValid(player: EntityPlayer): boolean {
-    const bombs = player.GetNumBombs();
-    return bombs < 50;
-  }
-
   override onAdd(player: EntityPlayer): void {
-    const numBombs = player.GetNumBombs();
-
-    g.run.babyCounters = numBombs; // Store the bomb count so that we can restore it later.
-    player.AddBombs(99);
-  }
-
-  /** Restore the bomb count to what it was before we got this baby. */
-  override onRemove(player: EntityPlayer, oldBabyCounters: int): void {
-    player.AddBombs(-99);
-    player.AddBombs(oldBabyCounters);
-  }
-
-  /** Infinite bombs. */
-  @CallbackCustom(ModCallbackCustom.POST_PEFFECT_UPDATE_REORDERED)
-  postPEffectUpdateReordered(player: EntityPlayer): void {
-    player.AddBombs(1);
+    player.AddGoldenBomb();
   }
 }
