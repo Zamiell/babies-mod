0ef8b4a4ce98edae34bd8a25e3192703ccf00f8f
James <5511220+Zamiell@users.noreply.github.com>
2023-02-15 14:42:13 -0500

    fix: build

--- src/mod.ts
+++ src/mod.ts
@@ -1,4 +1,4 @@
-import { ISCFeature, ModCallbackCustom, upgradeMod } from "isaacscript-common";
+import { ISCFeature, upgradeMod } from "isaacscript-common";
 import { MOD_NAME } from "./constants";
 
 const ISC_FEATURES_FOR_THIS_MOD = [
@@ -9,25 +9,5 @@ const ISC_FEATURES_FOR_THIS_MOD = [
   ISCFeature.SPAWN_COLLECTIBLE,
 ] as const;
 
-const CUSTOM_CALLBACKS_USED = [
-  ModCallbackCustom.POST_GAME_STARTED_REORDERED,
-  ModCallbackCustom.POST_NEW_LEVEL_REORDERED,
-  ModCallbackCustom.POST_NEW_ROOM_REORDERED,
-  ModCallbackCustom.POST_PICKUP_COLLECT,
-  ModCallbackCustom.PRE_ITEM_PICKUP,
-  ModCallbackCustom.POST_ITEM_PICKUP,
-  ModCallbackCustom.POST_PURCHASE,
-  ModCallbackCustom.POST_PLAYER_CHANGE_TYPE,
-  ModCallbackCustom.POST_SLOT_DESTROYED,
-  ModCallbackCustom.POST_TRINKET_BREAK,
-  ModCallbackCustom.POST_GRID_ENTITY_INIT,
-  ModCallbackCustom.POST_GRID_ENTITY_UPDATE,
-  ModCallbackCustom.POST_GRID_ENTITY_BROKEN,
-] as const;
-
 const modVanilla = RegisterMod(MOD_NAME, 1);
-export const mod = upgradeMod(
-  modVanilla,
-  ISC_FEATURES_FOR_THIS_MOD,
-  CUSTOM_CALLBACKS_USED,
-);
+export const mod = upgradeMod(modVanilla, ISC_FEATURES_FOR_THIS_MOD);
