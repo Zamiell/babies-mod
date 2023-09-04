41427a3ef7c2eb9f02ff46e411fffab21edf00d8
James <5511220+Zamiell@users.noreply.github.com>
2023-09-04 16:47:28 -0400

    feat: more effects for Gold Baby

--- src/classes/babies/GoldBaby.ts
+++ src/classes/babies/GoldBaby.ts
@@ -1,14 +1,25 @@
+import type { TrinketType } from "isaac-typescript-definitions";
 import {
   BombSubType,
   BombVariant,
   CoinSubType,
   EntityType,
   GridEntityType,
+  HeartSubType,
   KeySubType,
+  ModCallback,
   PickupVariant,
   PoopGridEntityVariant,
 } from "isaac-typescript-definitions";
-import { CallbackCustom, ModCallbackCustom, game } from "isaacscript-common";
+import {
+  Callback,
+  CallbackCustom,
+  ModCallbackCustom,
+  game,
+  getGoldenTrinketType,
+  isChestVariant,
+  isGoldenTrinketType,
+} from "isaacscript-common";
 import { Baby } from "../Baby";
 
 /** Gold gear + gold pickups + gold poops + gold rooms. */
@@ -17,6 +28,31 @@ export class GoldBaby extends Baby {
     player.AddGoldenHearts(12);
     player.AddGoldenBomb();
     player.AddGoldenKey();
+  }
+
+  // 66
+  @Callback(ModCallback.GET_TRINKET)
+  getTrinket(trinketType: TrinketType, _rng: RNG): TrinketType | undefined {
+    return isGoldenTrinketType(trinketType)
+      ? undefined
+      : getGoldenTrinketType(trinketType);
+  }
+
+  @CallbackCustom(
+    ModCallbackCustom.PRE_ENTITY_SPAWN_FILTER,
+    EntityType.PICKUP,
+    PickupVariant.HEART,
+  )
+  preEntitySpawnHeart(
+    entityType: EntityType,
+    variant: int,
+    _subType: int,
+    _position: Vector,
+    _velocity: Vector,
+    _spawner: Entity | undefined,
+    initSeed: Seed,
+  ): [EntityType, int, int, int] | undefined {
+    return [entityType, variant, HeartSubType.GOLDEN, initSeed];
   }
 
   @CallbackCustom(
@@ -68,6 +104,25 @@ export class GoldBaby extends Baby {
     initSeed: Seed,
   ): [EntityType, int, int, int] | undefined {
     return [entityType, variant, KeySubType.GOLDEN, initSeed];
+  }
+
+  @CallbackCustom(ModCallbackCustom.PRE_ENTITY_SPAWN_FILTER, EntityType.PICKUP)
+  preEntitySpawnChest(
+    entityType: EntityType,
+    variant: int,
+    _subType: int,
+    _position: Vector,
+    _velocity: Vector,
+    _spawner: Entity | undefined,
+    initSeed: Seed,
+  ): [EntityType, int, int, int] | undefined {
+    const pickupVariant = variant as PickupVariant;
+
+    if (isChestVariant(pickupVariant)) {
+      return [entityType, PickupVariant.LOCKED_CHEST, 0, initSeed];
+    }
+
+    return undefined;
   }
 
   @CallbackCustom(
