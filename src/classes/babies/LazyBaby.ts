0498b26fcbc3e8e80eff827bbd04adf468f1e849
Gamonymous <33094562+Rchardon@users.noreply.github.com>
2023-09-04 23:55:32 +0200

    Update LazyBaby.ts (#16)

--- src/classes/babies/LazyBaby.ts
+++ src/classes/babies/LazyBaby.ts
@@ -8,7 +8,7 @@ import { Baby } from "../Baby";
 export class LazyBaby extends Baby {
   @CallbackCustom(ModCallbackCustom.ENTITY_TAKE_DMG_PLAYER)
   entityTakeDmgPlayer(player: EntityPlayer): boolean | undefined {
-    const exceptions = [CardType.SUICIDE_KING]; // It would be unfair to randomly die.
+    const exceptions = [CardType.SUICIDE_KING, CardType.SOUL_LAZARUS]; // It would be unfair to randomly die.
     const card = mod.getRandomCard(g.run.rng, exceptions);
 
     // We don't use the `useCardTemp` helper function because we want the random card name to appear
