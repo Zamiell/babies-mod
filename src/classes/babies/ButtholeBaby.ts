a5d53cd906c17bede30c366eb71a4f12adcb8a17
James <5511220+Zamiell@users.noreply.github.com>
2023-09-04 19:36:00 -0400

    feat: butthole baby has more random poops

--- src/classes/babies/ButtholeBaby.ts
+++ src/classes/babies/ButtholeBaby.ts
@@ -1,5 +1,7 @@
 import {
+  EntityType,
   GridEntityType,
+  PoopEntityVariant,
   PoopGridEntityVariant,
   SoundEffect,
 } from "isaac-typescript-definitions";
@@ -7,14 +9,35 @@ import {
   CallbackCustom,
   ModCallbackCustom,
   game,
+  getEnumLength,
+  getRandomArrayElement,
+  getRandomEnumValue,
   getRandomInt,
+  newRNG,
+  setSeed,
   sfxManager,
+  spawnWithSeed,
 } from "isaacscript-common";
-import { g } from "../../globals";
 import { everyNSeconds } from "../../utils";
 import { Baby } from "../Baby";
 
+const NUM_POOP_ENTITY_VARIANTS = getEnumLength(PoopEntityVariant);
+if (NUM_POOP_ENTITY_VARIANTS !== 8) {
+  error(
+    "Butthole Baby needs to be updated to handle the new poop entity variants.",
+  );
+}
+const POOP_GRID_ENTITY_VARIANTS_NOT_IN_POOP_ENTITY_VARIANTS = [
+  PoopGridEntityVariant.RED, // 1
+  PoopGridEntityVariant.RAINBOW, // 4
+  PoopGridEntityVariant.CHARMING, // 11
+] as const;
+
 const v = {
+  run: {
+    poopRNG: newRNG(),
+  },
+
   room: {
     invulnerabilityUntilFrame: null as int | null,
   },
@@ -23,6 +46,12 @@ const v = {
 /** Spawns a random poop every N seconds. */
 export class ButtholeBaby extends Baby {
   v = v;
+
+  override onAdd(): void {
+    const level = game.GetLevel();
+    const seed = level.GetDungeonPlacementSeed();
+    setSeed(v.run.poopRNG, seed);
+  }
 
   @CallbackCustom(ModCallbackCustom.ENTITY_TAKE_DMG_PLAYER)
   entityTakeDmgPlayer(): boolean | undefined {
@@ -44,22 +73,45 @@ export class ButtholeBaby extends Baby {
     const num = this.getAttribute("num");
 
     everyNSeconds(() => {
-      const poopVariant = getRandomInt(
-        PoopGridEntityVariant.NORMAL,
-        PoopGridEntityVariant.WHITE,
-        g.run.rng,
-      ) as PoopGridEntityVariant;
+      // We want each poop type to have an equal chance of spawning.
+      const poopRoll = getRandomInt(
+        1,
+        NUM_POOP_ENTITY_VARIANTS +
+          POOP_GRID_ENTITY_VARIANTS_NOT_IN_POOP_ENTITY_VARIANTS.length,
+        v.run.poopRNG,
+      );
 
-      if (
-        poopVariant === PoopGridEntityVariant.RED ||
-        poopVariant === PoopGridEntityVariant.CORN
-      ) {
-        // If the poop is this type, it will instantly damage the player, so give them some
-        // invulnerability frames.
-        v.room.invulnerabilityUntilFrame = gameFrameCount + 25;
+      // Depending on the type of poop selected, we might need to spawn an entity or a grid entity.
+      if (poopRoll <= NUM_POOP_ENTITY_VARIANTS) {
+        const poopEntityVariant = getRandomEnumValue(
+          PoopEntityVariant,
+          v.run.poopRNG,
+        );
+        spawnWithSeed(
+          EntityType.POOP,
+          poopEntityVariant,
+          0,
+          player.Position,
+          v.run.poopRNG,
+        );
+      } else {
+        const poopGridEntityVariant = getRandomArrayElement(
+          POOP_GRID_ENTITY_VARIANTS_NOT_IN_POOP_ENTITY_VARIANTS,
+          v.run.poopRNG,
+        );
+
+        // Red Poops will instantly damage the player, so we have to give them some invulnerability
+        // frames.
+        if (poopGridEntityVariant === PoopGridEntityVariant.RED) {
+          v.room.invulnerabilityUntilFrame = gameFrameCount + 25;
+        }
+
+        Isaac.GridSpawn(
+          GridEntityType.POOP,
+          poopGridEntityVariant,
+          player.Position,
+        );
       }
-
-      Isaac.GridSpawn(GridEntityType.POOP, poopVariant, player.Position);
 
       sfxManager.Play(SoundEffect.FART);
     }, num);
