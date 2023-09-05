8e213d628b99f35d28f074596ed7990d64f2c0fb
James <5511220+Zamiell@users.noreply.github.com>
2023-09-05 07:53:00 -0400

    feat: Fishman Baby can make giga bombs

--- src/classes/babies/FishmanBaby.ts
+++ src/classes/babies/FishmanBaby.ts
@@ -1,5 +1,10 @@
 import { BombSubType, ModCallback } from "isaac-typescript-definitions";
-import { Callback, game, spawnBombPickupWithSeed } from "isaacscript-common";
+import {
+  Callback,
+  game,
+  getRandom,
+  spawnBombPickupWithSeed,
+} from "isaacscript-common";
 import { Baby } from "../Baby";
 
 /** Spawns a random bomb on room clear. */
@@ -10,7 +15,12 @@ export class FishmanBaby extends Baby {
     const roomSeed = room.GetSpawnSeed();
     const player = Isaac.GetPlayer();
 
-    spawnBombPickupWithSeed(BombSubType.NULL, player.Position, roomSeed);
+    // A bomb sub-type of `NULL` will never spawn a Giga Bomb, so we provide a small chance of that
+    // happening. (We want it to be slightly lower than the chance for a Golden Troll Bomb, which is
+    // the most unlikely thing in vanilla.)
+    const chance = getRandom(roomSeed);
+    const bombSubType = chance < 0.001 ? BombSubType.GIGA : BombSubType.NULL;
+    spawnBombPickupWithSeed(bombSubType, player.Position, roomSeed);
 
     return undefined;
   }
