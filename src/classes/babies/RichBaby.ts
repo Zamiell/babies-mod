67b863a1de1221b2f29b15dac4cdf2eb536dd941
James <5511220+Zamiell@users.noreply.github.com>
2023-09-05 17:24:09 -0400

    refactor: Rich Baby

--- src/classes/babies/RichBaby.ts
+++ src/classes/babies/RichBaby.ts
@@ -1,25 +1,29 @@
 import { RoomType } from "isaac-typescript-definitions";
 import { levelHasRoomType } from "isaacscript-common";
-import { g } from "../../globals";
 import { Baby } from "../Baby";
+
+const v = {
+  run: {
+    originalNumCoins: 0,
+  },
+};
 
 /** Starts with 99 cents. */
 export class RichBaby extends Baby {
+  v = v;
+
   override isValid(): boolean {
     return levelHasRoomType(RoomType.SHOP);
   }
 
   override onAdd(player: EntityPlayer): void {
-    const numCoins = player.GetNumCoins();
-
-    g.run.babyCounters = numCoins;
+    v.run.originalNumCoins = player.GetNumCoins();
     player.AddCoins(99);
   }
 
-  override onRemove(player: EntityPlayer, oldBabyCounters: int): void {
+  override onRemove(player: EntityPlayer): void {
     const numCoins = player.GetNumCoins();
-    const oldNumCoins = oldBabyCounters;
-    const difference = oldNumCoins - numCoins;
+    const difference = v.run.originalNumCoins - numCoins;
 
     if (difference < 0) {
       player.AddCoins(difference);
