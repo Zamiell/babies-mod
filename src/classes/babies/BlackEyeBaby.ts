cab74d8abd560f2e5d33519288b6cdb0913e3e5d
James <5511220+Zamiell@users.noreply.github.com>
2023-09-06 14:51:09 -0400

    refactor: Black Eye Baby

--- src/classes/babies/BlackEyeBaby.ts
+++ src/classes/babies/BlackEyeBaby.ts
@@ -9,17 +9,24 @@ import {
   ModCallbackCustom,
   getFamiliars,
 } from "isaacscript-common";
-import { g } from "../../globals";
 import { Baby } from "../Baby";
 
-/** Starts with Leprosy, +5 damage on Leprosy breaking. */
+const v = {
+  run: {
+    numLeprosyChunks: 0,
+    numLeprosyChunksBroken: 0,
+  },
+};
+
+/** Starts with Leprosy, +N damage on Leprosy breaking. */
 export class BlackEyeBaby extends Baby {
+  v = v;
+
   // 7
   @Callback(ModCallback.POST_FAMILIAR_INIT, FamiliarVariant.LEPROSY)
   postFamiliarInitLeprosy(): void {
-    if (g.run.babyCounters < 3) {
-      // We use the "babyCounters" variable to track how Leprocy familiars are in the room.
-      g.run.babyCounters++;
+    if (v.run.numLeprosyChunks < 3) {
+      v.run.numLeprosyChunks++;
     }
   }
 
@@ -27,18 +34,16 @@ export class BlackEyeBaby extends Baby {
   @Callback(ModCallback.EVALUATE_CACHE, CacheFlag.DAMAGE)
   evaluateCacheDamage(player: EntityPlayer): void {
     const num = this.getAttribute("num");
-    player.Damage += g.run.babyFrame * num;
+    player.Damage += v.run.numLeprosyChunksBroken * num;
   }
 
   @CallbackCustom(ModCallbackCustom.POST_PEFFECT_UPDATE_REORDERED)
   postPEffectUpdateReordered(player: EntityPlayer): void {
-    // We use the "babyCounters" variable to track how Leprocy familiars are in the room.
+    // We use the "babyCounters" variable to track how many Leprocy familiars are in the room.
     const leprocyChunks = getFamiliars(FamiliarVariant.LEPROSY);
-    if (leprocyChunks.length < g.run.babyCounters) {
-      g.run.babyCounters--;
-
-      // We use the "babyFrame" variable to track how many damage ups we have received.
-      g.run.babyFrame++;
+    if (leprocyChunks.length < v.run.numLeprosyChunks) {
+      v.run.numLeprosyChunks--;
+      v.run.numLeprosyChunksBroken++;
       player.AddCacheFlags(CacheFlag.DAMAGE);
       player.EvaluateItems();
     }
