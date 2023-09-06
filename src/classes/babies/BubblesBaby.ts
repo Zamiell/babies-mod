f023c4e341728258064526c34e112e376fc78a2f
James <5511220+Zamiell@users.noreply.github.com>
2023-09-06 17:54:26 -0400

    refactor: Bubbles Baby

--- src/classes/babies/BubblesBaby.ts
+++ src/classes/babies/BubblesBaby.ts
@@ -1,15 +1,22 @@
 import type { PillEffect } from "isaac-typescript-definitions";
 import { CacheFlag, ModCallback } from "isaac-typescript-definitions";
 import { Callback, repeat } from "isaacscript-common";
-import { g } from "../../globals";
 import { Baby } from "../Baby";
+
+const v = {
+  run: {
+    pillsUsed: 0,
+  },
+};
 
 /** +1 damage per pill used. */
 export class BubblesBaby extends Baby {
+  v = v;
+
   // 8
   @Callback(ModCallback.EVALUATE_CACHE, CacheFlag.DAMAGE)
   evaluateCacheDamage(player: EntityPlayer): void {
-    repeat(g.run.babyCounters, () => {
+    repeat(v.run.pillsUsed, () => {
       player.Damage++;
     });
   }
@@ -17,7 +24,8 @@ export class BubblesBaby extends Baby {
   // 8
   @Callback(ModCallback.POST_USE_PILL)
   postUsePill(_pillEffect: PillEffect, player: EntityPlayer): void {
-    g.run.babyCounters++;
+    v.run.pillsUsed++;
+
     player.AddCacheFlags(CacheFlag.DAMAGE);
     player.EvaluateItems();
   }
