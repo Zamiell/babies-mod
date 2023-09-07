35657bb0b64f2bbc13f7e2231c0b6a4fad3c9fd2
James <5511220+Zamiell@users.noreply.github.com>
2023-09-06 22:45:59 -0400

    refactor: SoftlockPrevention

--- src/classes/features/SoftlockPrevention.ts
+++ src/classes/features/SoftlockPrevention.ts
@@ -35,76 +35,76 @@ export class SoftlockPrevention extends BabyModFeature {
     }
     const { baby } = currentBaby;
 
-    checkSoftlockDestroyPoopsTNT(baby);
-    checkSoftlockIsland(baby);
-  }
-}
-
-/**
- * On certain babies, destroy all poops and TNT barrels after a certain amount of time to prevent
- * softlocks.
- */
-function checkSoftlockDestroyPoopsTNT(baby: BabyDescription) {
-  const room = game.GetRoom();
-  const roomFrameCount = room.GetFrameCount();
-
-  if (baby.softlockPreventionDestroyPoops !== true) {
-    return;
+    this.checkSoftlockDestroyPoopsTNT(baby);
+    this.checkSoftlockIsland(baby);
   }
 
-  // Check to see if we already destroyed the grid entities in the room.
-  if (v.room.destroyedGridEntities) {
-    return;
+  /**
+   * On certain babies, destroy all poops and TNT barrels after a certain amount of time to prevent
+   * softlocks.
+   */
+  checkSoftlockDestroyPoopsTNT(baby: BabyDescription): void {
+    const room = game.GetRoom();
+    const roomFrameCount = room.GetFrameCount();
+
+    if (baby.softlockPreventionDestroyPoops !== true) {
+      return;
+    }
+
+    // Check to see if we already destroyed the grid entities in the room.
+    if (v.room.destroyedGridEntities) {
+      return;
+    }
+
+    // Check to see if they have been in the room long enough.
+    const secondsThreshold = 15;
+    if (roomFrameCount < secondsThreshold * GAME_FRAMES_PER_SECOND) {
+      return;
+    }
+
+    v.room.destroyedGridEntities = true;
+
+    // Kill some grid entities in the room to prevent softlocks in some specific rooms. (Fireplaces
+    // will not cause softlocks since they are killable with The Candle.)
+    const gridEntities = getGridEntities(
+      GridEntityType.TNT, // 12
+      GridEntityType.POOP, // 14
+    );
+    for (const gridEntity of gridEntities) {
+      gridEntity.Destroy(true);
+    }
+
+    log("Destroyed all poops & TNT barrels to prevent a softlock.");
   }
 
-  // Check to see if they have been in the room long enough.
-  const secondsThreshold = 15;
-  if (roomFrameCount < secondsThreshold * GAME_FRAMES_PER_SECOND) {
-    return;
+  /**
+   * On certain babies, open the doors after 30 seconds to prevent softlocks with Hosts and island
+   * enemies.
+   */
+  checkSoftlockIsland(baby: BabyDescription): void {
+    const room = game.GetRoom();
+    const roomFrameCount = room.GetFrameCount();
+
+    // Check to see if this baby needs the softlock prevention.
+    if (baby.softlockPreventionIsland === undefined) {
+      return;
+    }
+
+    // Check to see if we already opened the doors in the room.
+    if (v.room.openedDoors) {
+      return;
+    }
+
+    // Check to see if they have been in the room long enough.
+    const secondsThreshold = 30;
+    if (roomFrameCount < secondsThreshold * GAME_FRAMES_PER_SECOND) {
+      return;
+    }
+
+    v.room.openedDoors = true;
+    room.SetClear(true);
+    openAllDoors();
+
+    log("Opened all doors to prevent a softlock.");
   }
-
-  v.room.destroyedGridEntities = true;
-
-  // Kill some grid entities in the room to prevent softlocks in some specific rooms. (Fireplaces
-  // will not cause softlocks since they are killable with The Candle.)
-  const gridEntities = getGridEntities(
-    GridEntityType.TNT, // 12
-    GridEntityType.POOP, // 14
-  );
-  for (const gridEntity of gridEntities) {
-    gridEntity.Destroy(true);
-  }
-
-  log("Destroyed all poops & TNT barrels to prevent a softlock.");
-}
-
-/**
- * On certain babies, open the doors after 30 seconds to prevent softlocks with Hosts and island
- * enemies.
- */
-function checkSoftlockIsland(baby: BabyDescription) {
-  const room = game.GetRoom();
-  const roomFrameCount = room.GetFrameCount();
-
-  // Check to see if this baby needs the softlock prevention.
-  if (baby.softlockPreventionIsland === undefined) {
-    return;
-  }
-
-  // Check to see if we already opened the doors in the room.
-  if (v.room.openedDoors) {
-    return;
-  }
-
-  // Check to see if they have been in the room long enough.
-  const secondsThreshold = 30;
-  if (roomFrameCount < secondsThreshold * GAME_FRAMES_PER_SECOND) {
-    return;
-  }
-
-  v.room.openedDoors = true;
-  room.SetClear(true);
-  openAllDoors();
-
-  log("Opened all doors to prevent a softlock.");
 }
