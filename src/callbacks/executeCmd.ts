4b6b513e3ad2fb70b6eade9abb11e41ec635fcc5
James <5511220+Zamiell@users.noreply.github.com>
2023-09-07 00:35:26 -0400

    refactor: delete executeCmdFunctionMap

--- src/callbacks/executeCmd.ts
+++ src/callbacks/executeCmd.ts
@@ -1,6 +1,7 @@
 import { ModCallback } from "isaac-typescript-definitions";
+import { debugFunction, setDebugBaby } from "../debugFunction";
+import { g } from "../globals";
 import { mod } from "../mod";
-import { executeCmdFunctionMap } from "./executeCmdFunctionMap";
 
 export function init(): void {
   mod.AddCallback(ModCallback.EXECUTE_CMD, main);
@@ -12,3 +13,24 @@ function main(cmd: string, params: string) {
     executeCmdFunction(params);
   }
 }
+
+const executeCmdFunctionMap = new Map<string, (params: string) => void>();
+
+// "baby #" will restart as the specified baby.
+executeCmdFunctionMap.set("baby", (params: string) => {
+  setDebugBaby(params, true);
+});
+
+// "baby2" will lock in the specified baby but will not restart the current run.
+executeCmdFunctionMap.set("baby2", (params: string) => {
+  setDebugBaby(params, false);
+});
+
+executeCmdFunctionMap.set("babydebug", (_params: string) => {
+  debugFunction();
+});
+
+executeCmdFunctionMap.set("disable", (_params: string) => {
+  g.debugBabyNum = undefined;
+  Isaac.ExecuteCommand("restart");
+});
