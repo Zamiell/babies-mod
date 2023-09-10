dc7b8f7f2eabc788bcaa30d03893dce252999d91
Gamonymous <33094562+Rchardon@users.noreply.github.com>
2023-09-11 01:03:11 +0200

    More exclusions (#31)
    * More exclusions
    
    * Update babies.ts
    
    ---------
    
    Co-authored-by: James <5511220+Zamiell@users.noreply.github.com>

--- src/classes/babies/BlueGhostBaby.ts
+++ src/classes/babies/BlueGhostBaby.ts
@@ -9,7 +9,11 @@ import { Baby } from "../Baby";
 /** Max tear rate. */
 export class BlueGhostBaby extends Baby {
   override isValid(player: EntityPlayer): boolean {
-    return !player.HasCollectible(CollectibleType.MOMS_KNIFE);
+    return (
+      !player.HasCollectible(CollectibleType.MOMS_KNIFE) &&
+      !player.HasCollectible(CollectibleType.EPIC_FETUS) &&
+      !player.HasCollectible(CollectibleType.SPIRIT_SWORD)
+    );
   }
 
   @Callback(ModCallback.EVALUATE_CACHE, CacheFlag.FIRE_DELAY)
