6518103f0fb61e36088c3098aa2cfccf7917785b
James <5511220+Zamiell@users.noreply.github.com>
2023-09-10 16:00:12 -0400

    Update SpiderBaby.ts

--- src/classes/babies/SpiderBaby.ts
+++ src/classes/babies/SpiderBaby.ts
@@ -19,8 +19,7 @@ export class SpiderBaby extends Baby {
   v = v;
 
   override isValid(player: EntityPlayer): boolean {
-    // C Section reduces the tear rate for no additional tears unlike Monstro's lung. So the custom
-    // effect with C Section is a straight-up downgrade.
+    // The custom effect with C Section is a downgrade. (But Monstro's Lung is okay.)
     return !player.HasCollectible(CollectibleType.C_SECTION);
   }
 
