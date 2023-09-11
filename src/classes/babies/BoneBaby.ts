ce53ae13cb0b39065fb973144907b8963ab399f1
James <5511220+Zamiell@users.noreply.github.com>
2023-09-11 05:56:46 -0400

    feat: new Bone Baby effect

--- src/classes/babies/BoneBaby.ts
+++ src/classes/babies/BoneBaby.ts
@@ -1,13 +1,41 @@
-import { ModCallback } from "isaac-typescript-definitions";
-import { Callback } from "isaacscript-common";
+import { DisplayFlag, ModCallback } from "isaac-typescript-definitions";
+import {
+  Callback,
+  addRoomDisplayFlag,
+  getRandomArrayElement,
+  getRoomsInsideGrid,
+  isRoomVisible,
+  newRNG,
+} from "isaacscript-common";
+import { setInitialBabyRNG } from "../../utils";
 import { Baby } from "../Baby";
+
+const v = {
+  run: {
+    rng: newRNG(),
+  },
+};
 
 /** Reveals a random room on room clear. */
 export class BoneBaby extends Baby {
+  v = v;
+
+  override onAdd(): void {
+    setInitialBabyRNG(v.run.rng);
+  }
+
   @Callback(ModCallback.PRE_SPAWN_CLEAR_AWARD)
   preSpawnClearAward(): boolean | undefined {
-    /// const roomsInsideGrid = getRoomsInsideGrid();
-    /// const unrevealedRooms = roomsInsideGrid.filter(roomDescriptor => roomDescriptor.DisplayFlags)
+    const roomsInsideGrid = getRoomsInsideGrid();
+    const nonVisibleRooms = roomsInsideGrid.filter(
+      (roomDescriptor) => !isRoomVisible(roomDescriptor),
+    );
+    if (nonVisibleRooms.length === 0) {
+      return;
+    }
+
+    const randomRoom = getRandomArrayElement(nonVisibleRooms, v.run.rng);
+    addRoomDisplayFlag(randomRoom.SafeGridIndex, DisplayFlag.VISIBLE);
 
     return undefined;
   }
