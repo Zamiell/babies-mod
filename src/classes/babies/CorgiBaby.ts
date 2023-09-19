d88a68abc0da4aa30562a330d0cc37d45959416e
James <5511220+Zamiell@users.noreply.github.com>
2023-09-19 12:28:27 -0400

    fix: Corgi Baby + Ipecac

--- src/classes/babies/CorgiBaby.ts
+++ src/classes/babies/CorgiBaby.ts
@@ -1,10 +1,15 @@
-import { EntityType } from "isaac-typescript-definitions";
+import { CollectibleType, EntityType } from "isaac-typescript-definitions";
 import { CallbackCustom, ModCallbackCustom, spawn } from "isaacscript-common";
 import { everyNSeconds } from "../../utils";
 import { Baby } from "../Baby";
 
 /** Spawns a fly every N seconds. */
 export class CorgiBaby extends Baby {
+  /** Certain collectibles make the baby too dangerous. */
+  override isValid(player: EntityPlayer): boolean {
+    return !player.HasCollectible(CollectibleType.IPECAC);
+  }
+
   @CallbackCustom(ModCallbackCustom.POST_PEFFECT_UPDATE_REORDERED)
   postPEffectUpdateReordered(player: EntityPlayer): void {
     const num = this.getAttribute("num");
