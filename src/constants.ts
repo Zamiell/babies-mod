7354b36080dd61794e14dfcec0751e940e6d29b5
James <5511220+Zamiell@users.noreply.github.com>
2023-10-02 20:09:48 -0400

    chore: release 1.25.3

--- src/constants.ts
+++ src/constants.ts
@@ -13,7 +13,7 @@ import { RandomBabyType } from "./enums/RandomBabyType";
 export const MOD_NAME = "The Babies Mod";
 
 /** The version is updated automatically by IsaacScript. */
-export const VERSION = "1.25.2";
+export const VERSION = "1.25.3";
 
 export const IS_DEV = false as boolean;
 
