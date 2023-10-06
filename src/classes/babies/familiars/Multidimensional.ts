a9bc5c5cf57a70c90107a30e948e2150fcea840b
James <5511220+Zamiell@users.noreply.github.com>
2023-10-06 15:24:56 -0400

    fix: multidimensional baby crashes

--- src/classes/babies/familiars/Multidimensional.ts
+++ src/classes/babies/familiars/Multidimensional.ts
@@ -1,12 +1,20 @@
 import { EntityType, ModCallback } from "isaac-typescript-definitions";
-import { Callback, VectorZero, repeat, spawn } from "isaacscript-common";
+import { Callback, VectorZero, game, repeat, spawn } from "isaacscript-common";
 import { isValidForEnemyDeathEffect } from "../../../utils";
 import { Baby } from "../../Baby";
 
 const SWARM_SPIDER_DISPLACEMENT_DISTANCE = 3;
 
+const v = {
+  run: {
+    spawnedSwarmSpidersGameFrame: 0,
+  },
+};
+
 /** Enemies spawn N Swarm Spiders on death. */
 export class Multidimensional extends Baby {
+  v = v;
+
   @Callback(ModCallback.POST_ENTITY_KILL)
   postEntityKill(entity: Entity): void {
     if (!isValidForEnemyDeathEffect(entity)) {
@@ -16,6 +24,12 @@ export class Multidimensional extends Baby {
     if (entity.Type === EntityType.SWARM_SPIDER) {
       return;
     }
+
+    const gameFrameCount = game.GetFrameCount();
+    if (gameFrameCount === v.run.spawnedSwarmSpidersGameFrame) {
+      return;
+    }
+    v.run.spawnedSwarmSpidersGameFrame = gameFrameCount;
 
     const num = this.getAttribute("num");
 
