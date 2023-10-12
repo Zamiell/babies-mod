27896a035938eecdc692ddf002969558ef90c2fc
James <5511220+Zamiell@users.noreply.github.com>
2023-10-12 12:31:46 -0400

    feat: VBaby exception

--- src/classes/babies/VBaby.ts
+++ src/classes/babies/VBaby.ts
@@ -1,12 +1,20 @@
-import { ModCallback } from "isaac-typescript-definitions";
-import { Callback } from "isaacscript-common";
+import { CollectibleType, ModCallback } from "isaac-typescript-definitions";
+import { Callback, hasCollectible } from "isaacscript-common";
 import { getBabyPlayerFromEntity } from "../../utils";
 import { Baby } from "../Baby";
 
 const RING_RADIUS = 5;
 
+const ANTI_SYNERGY_COLLECTIBLES = [
+  CollectibleType.C_SECTION, // 678
+] as const;
+
 /** Electric ring tears. */
 export class VBaby extends Baby {
+  override isValid(player: EntityPlayer): boolean {
+    return !hasCollectible(player, ...ANTI_SYNERGY_COLLECTIBLES);
+  }
+
   @Callback(ModCallback.POST_FIRE_TEAR)
   postFireTear(tear: EntityTear): void {
     const player = getBabyPlayerFromEntity(tear);
