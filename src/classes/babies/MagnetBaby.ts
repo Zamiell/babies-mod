6f8981c2adf1bf31b1f8f43d2b80562fbdd64843
Gamonymous <33094562+Rchardon@users.noreply.github.com>
2023-10-17 15:22:21 +0200

    Update MagnetBaby.ts

--- src/classes/babies/MagnetBaby.ts
+++ src/classes/babies/MagnetBaby.ts
@@ -1,13 +1,22 @@
 import {
+  LevelStage,
   ModCallback,
   TearFlag,
   TearVariant,
 } from "isaac-typescript-definitions";
-import { Callback, addFlag } from "isaacscript-common";
+import { Callback, addFlag, onStage } from "isaacscript-common";
 import { Baby } from "../Baby";
 
 /** Magnetizing tears. */
 export class MagnetBaby extends Baby {
+  override isValid(): boolean {
+    return (
+      // Magnetizing tears cause Blue Baby and eternal flies to attract each other, resulting of
+      // Blue Baby flying at very high speed around the room, causing unavoidable damage.
+      !onStage(LevelStage.DARK_ROOM_CHEST) && !onStage(LevelStage.BLUE_WOMB)
+    );
+  }
+
   @Callback(ModCallback.POST_FIRE_TEAR)
   postFireTear(tear: EntityTear): void {
     tear.ChangeVariant(TearVariant.METALLIC);
